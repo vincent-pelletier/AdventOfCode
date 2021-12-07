501b0f3efc0d5e37306605f954bc665edd2917db
Vincent Pelletier <pelletier.v@hotmail.com>
2021-12-06 22:53:42 -0500

    Refactor Day 6

--- src/test/java/Day6.java
+++ src/test/java/Day6.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.Test;
 
@@ -33,47 +33,20 @@ public class Day6 extends AdventOfCode {
 
 		br();
 
-		// loop to 256 days = OutOfMemoryError: Java heap space...
-		// so find result and growth after half, then compute
-		int[] growthAfter128 = new int[9];
-		long[][] countAfter128 = new long[9][9];
+		// loop to 256 days = OutOfMemoryError: Java heap space... change tactic
+		List<AtomicLong> fishPerAge = new ArrayList<>();
 		for(int i = 0; i < 9; i++) {
-			fishes.clear();
-			fishes.add(new AtomicInteger(i)); // add only 1 fish of "i" days
-			for(int j = 0; j < 128; j++) {
-				List<AtomicInteger> newFishes = new ArrayList<>();
-				for(AtomicInteger fish : fishes) {
-					if(fish.get() > 0) {
-						fish.decrementAndGet();
-					} else {
-						fish.set(6);
-						newFishes.add(new AtomicInteger(8));
-					}
-				}
-				fishes.addAll(newFishes);
-			}
-			// print("In 128 days, " + i + " becomes " + fishes.size());
-			growthAfter128[i] = fishes.size();
-			for(AtomicInteger fish : fishes) {
-				countAfter128[i][fish.get()]++;
-			}
+			fishPerAge.add(new AtomicLong(0L));
+		}
+		for(Integer fish : initialFishes) {
+			fishPerAge.get(fish).incrementAndGet();
 		}
 
-		int[] counts = new int[9];
-		counts[1] = initialFishes.stream().filter(f -> f == 1).collect(Collectors.toList()).size();
-		counts[2] = initialFishes.stream().filter(f -> f == 2).collect(Collectors.toList()).size();
-		counts[3] = initialFishes.stream().filter(f -> f == 3).collect(Collectors.toList()).size();
-		counts[4] = initialFishes.stream().filter(f -> f == 4).collect(Collectors.toList()).size();
-		counts[5] = initialFishes.stream().filter(f -> f == 5).collect(Collectors.toList()).size();
-
-		long sum = 0L;
-		for(int i = 0; i < 9; i++) {
-			long sumPerFish = 0L;
-			for(int j = 0; j < 9; j++) { // compute amount of each new fish after 128 days
-				sumPerFish += countAfter128[i][j] * growthAfter128[j];
-			}
-			sum += sumPerFish * counts[i]; // x initial amount
+		for(int i = 0; i < 256; i++) {
+			AtomicLong fishes0 = fishPerAge.remove(0);
+			fishPerAge.get(6).set(fishPerAge.get(6).get() + fishes0.get());
+			fishPerAge.add(new AtomicLong(fishes0.get()));
 		}
-		print(sum);
+		print(fishPerAge.stream().mapToLong(AtomicLong::get).sum());
 	}
 }
