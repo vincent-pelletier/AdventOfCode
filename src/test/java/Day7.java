803da2ee3fbdd6f57027508cb4dc25ed2f5e1de6
Vincent Pelletier <pelletier.v@hotmail.com>
2021-12-07 00:23:47 -0500

    Day 7

---
+++ src/test/java/Day7.java
@@ -0,0 +1,43 @@
+import java.util.List;
+
+import org.junit.Test;
+
+public class Day7 extends AdventOfCode {
+
+	@Test
+	public void day7() {
+		printHeader();
+		List<Integer> crabs = readSingleLineAsInts("day7.txt");
+
+		int max = crabs.stream().max(Integer::compareTo).get();
+		int min = crabs.stream().min(Integer::compareTo).get();
+		int lowestFuel = Integer.MAX_VALUE;
+		for(int i = min; i < max; i++) {
+			int fuel = 0;
+			for(Integer crab : crabs) {
+				fuel += Math.abs(crab - i);
+			}
+			if(fuel < lowestFuel) {
+				lowestFuel = fuel;
+			}
+		}
+		print(lowestFuel);
+
+		br();
+
+		lowestFuel = Integer.MAX_VALUE;
+		for(int i = min; i < max; i++) {
+			int fuel = 0;
+			for(Integer crab : crabs) {
+				int diff = Math.abs(crab - i);
+				for(int j = 0; j < diff; j++) {
+					fuel += (j+1);
+				}
+			}
+			if(fuel < lowestFuel) {
+				lowestFuel = fuel;
+			}
+		}
+		print(lowestFuel);
+	}
+}
