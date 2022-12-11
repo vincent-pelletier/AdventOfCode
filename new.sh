a8d0e2685543de685cd5e7dc3ca269ec23fa63af
Vincent Pelletier <pelletier.v@hotmail.com>
2022-12-11 15:23:00 -0500

    Create bash script to generate daily template files
    ./new.sh 3

---
+++ new.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if (($# == 1)); then
+	YEAR=$(date +%Y)
+	DAY=$1
+	if (($DAY < 10)); then
+		DAY="0"$DAY
+	fi
+elif (($# == 2)); then
+	YEAR=$1
+	DAY=$2
+else
+	echo "Must specify year and day, or only day for current year:"
+	echo "	./new.sh 1"
+	echo "	./new.sh 2022 1"
+	exit
+fi
+
+touch src/test/resources/$YEAR/day$DAY.txt
+
+echo """package y$YEAR;
+
+import org.junit.Test;
+
+import common.AdventOfCode;
+
+public class Day$DAY extends AdventOfCode {
+
+	@Test
+	public void test() {
+
+	}
+}""" > src/test/java/y$YEAR/Day$DAY.java
+
+echo "Created template files for $YEAR-12-$DAY"
---
+++ src/test/java/y2022/Day03.java
@@ -0,0 +1,13 @@
+package y2022;
+
+import org.junit.Test;
+
+import common.AdventOfCode;
+
+public class Day03 extends AdventOfCode {
+
+	@Test
+	public void test() {
+
+	}
+}
